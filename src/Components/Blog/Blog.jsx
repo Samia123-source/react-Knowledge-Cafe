991d69d0ce014460d009f8626a70863d158ef70a
Samia123-source <samiamahim04@gmail.com>
2026-05-18 14:32:49 +0600

    removing boorkmark when mark as read

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -3,7 +3,7 @@ import { FaBookmark } from "react-icons/fa";
 
 
 const Blog = ({blog, handleAddToBookmark,  handleMarkAsRead }) => {
-   const{title, cover, author, author_img,  posted_date, 
+   const{id, title, cover, author, author_img,  posted_date, 
     reading_time, hashtags } = blog;
     return (
         <div className='mb-20 space-y-4' >
@@ -31,7 +31,7 @@ const Blog = ({blog, handleAddToBookmark,  handleMarkAsRead }) => {
                 }
             </p>
             <button
-            onClick = {() => handleMarkAsRead(reading_time)} 
+            onClick = {() => handleMarkAsRead(id, reading_time)} 
             className='text-purple-800 underline
             '>Mark As Read</button>
         </div>
